3ba86d05adb3a4362dd863530bfc7fb48a791aca
ChiantiWine <hsm11256@naver.com>
2024-12-08 23:20:35 +0900

    2024.12.08
    1. 결승선(Order in Layer) 조정
    - 카메라보다 뒤에 있으면 안보이기도 함(3D로 확인 가능)
    2. Particle Effect 트리거 설정(paly on awake, Simulation Space)

--- Assets/Script/CrashDetector.cs
+++ Assets/Script/CrashDetector.cs
@@ -3,13 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashEffect;
     [SerializeField] float loadDelay = 1f;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Ground"))
         {
             Debug.Log("Ouch");
+            crashEffect.Play();
             Invoke("roadScene", loadDelay);
+            
         }    
     }
 
--- Assets/Script/FinishLine.cs
+++ Assets/Script/FinishLine.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishEffect;
     [SerializeField] float finishDelay = 1f;
     bool hasTriggered = false;
 
@@ -10,6 +11,7 @@ public class FinishLine : MonoBehaviour
     {
         if(!hasTriggered && other.CompareTag("Player"))
         Debug.Log("Win");
+        finishEffect.Play();
        Invoke("roadScene", finishDelay);
         hasTriggered = true;
     }
