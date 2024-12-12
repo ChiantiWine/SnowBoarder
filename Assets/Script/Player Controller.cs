ee31d0bc7dd541416b2926fa37b426e6ba5e8828
ChiantiWine <hsm11256@naver.com>
2024-12-13 02:57:01 +0900

    2024.12.13
    1. Math.Abs()
    2. ForceMode.Impulse
    3. linearVelocityY
    4. 캐릭터 떨림(Interpolate)
    
    수정 및 보완
    1., 캐릭터 점프하기
    2. 바위 오브젝트
    3. 배경 꾸미기

--- Assets/Script/Player Controller.cs
+++ Assets/Script/Player Controller.cs
@@ -1,8 +1,12 @@
+using System.Numerics;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float jumpForce = 50f;
     Rigidbody2D rd2d;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,5 +22,21 @@ public class PlayerController : MonoBehaviour
         {
             rd2d.AddTorque(torqueAmount * horizontalInput);
         }
+        
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+           Debug.Log("Jump");
+        }
     }
+    void Jump()
+    {
+        // Y축 속도가 거의 0일 때만 점프하도록 조건 설정
+        if (Mathf.Abs(rd2d.linearVelocityY) < 0.01f)
+        {
+            rd2d.AddForce(new UnityEngine.Vector2(0, jumpForce), ForceMode2D.Impulse); // 점프 힘 추가
+        }
+    }
+    
 }
